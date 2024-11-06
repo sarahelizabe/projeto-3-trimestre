3510282aa9618886f43d025a711b6b39a7abd4e4
sarahelizabe <165155457+sarahelizabe@users.noreply.github.com>
2024-11-06 08:03:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Desenho animado' ,
+    'Quem é o vilão que esta sempre tentando capturar o "Pernalonga" nos desenhos da Warner Bros?',
+    'hortelino troca-letras'
+)
+
+criaCartao(
+    'Artes',
+    'Qual a obra  mais famosa de arte de Leonardo a Vinci retrata uma mulher com um sorriso enigmatico?',
+    'Monalisa'
+)
+
+criaCartao(
+    'Musica',
+    'O que é uma clave na notação musical?',
+    'A clave é um simbolo colocadono inicio de uma pauta musical que indica a altura das notas em relação'
+)
+
+criaCartao(
+    'Desenho Animado',
+    'Qal serie de desenho da Nickelodeon segue a aventuras de um garoto e um cachorro magico em uma terra após-apocaliptica',
+    'Hora de aventura'
+)
